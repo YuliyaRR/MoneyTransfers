4175704b5fe09dcc34c5aa193ca5fc2757382338
Yuliya Romashko <ramashkay@gmail.com>
2023-11-09 12:31:29 +0300

    Created: OpenApiConfig

---
+++ transfers/src/main/java/com/project/transfers/config/OpenApiConfig.java
@@ -0,0 +1,11 @@
+package com.project.transfers.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Money Transfers Api",
+                description = "Money Transfers", version = "1.0.0"))
+public class OpenApiConfig {
+}
