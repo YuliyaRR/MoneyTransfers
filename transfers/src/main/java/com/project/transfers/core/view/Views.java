42b6acc8dea767433b8600af07cbf9a74c43f041
Yuliya Romashko <ramashkay@gmail.com>
2023-11-09 12:31:02 +0300

    Created: views

---
+++ transfers/src/main/java/com/project/transfers/core/view/Views.java
@@ -0,0 +1,7 @@
+package com.project.transfers.core.view;
+
+public class Views {
+    public static class transactionalOutAccountFrom {}
+    public static class transactionalOutAccountTo {}
+    public static class transactionalOutTransfer {}
+}
